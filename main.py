892e3f14b2423e239ffb985b25065294825304fd
fabio.alves <fabio.alves@exablack.com>
2020-12-08 23:35:39 -0300

    Finally ploted a chart

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import yfinance as yf
-
+import matplotlib.pyplot as plt
 
 ticker = yf.Ticker('TSLA')
 tsla_df = ticker.history(period="max")
 tsla_df["Close"].plot(title="TSLA's stock price")
+
+plt.show()
