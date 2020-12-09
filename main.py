2cf03632f6d3e1cc745860204f5d2f89c04aa704
fabio.alves <fabio.alves@exablack.com>
2020-12-08 22:09:25 -0300

    trying to grasp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import yfinance as yf, pandas as pd, shutil, os, time, glob,smtplib, ssl
-from get_all_ticjers import get_tickers as gt
+import pandas as pd
+import yfinance as yf
 
-tickers = gt.get_tickers_filtered(mktcap_min=150000, mktcap_max=10000000)
-print("The amount of stockes chosen to oberse: " + str(len(tickers)))
 
+ticker = yf.Ticker('TSLA')
+tsla_df = ticker.history(period="max")
+tsla_df["Close"].plot(title="TSLA's stock price")
