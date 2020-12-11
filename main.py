0b6e368568190e04424074425ff0966e63de3302
fabio.alves <fabio.alves@exablack.com>
2020-12-11 15:20:27 -0300

    writting the same data, but inversed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ ticker = yf.Ticker('TSLA')
 tsla_df = ticker.history(period="max")
 tsla_df["Close"].plot(title="TSLA's stock price")
 
-tsla_df.to_csv(path_or_buf ="tesla.csv")
+tsla_df.iloc[::-1].to_csv(path_or_buf ="tesla.csv")
 
 
 plt.show()
