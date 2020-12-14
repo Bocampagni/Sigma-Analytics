79f5e8ce05c780f32637ef2864f25267cc1425bc
fabio.alves <fabio.alves@exablack.com>
2020-12-13 21:02:35 -0300

    getting info about the companies

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
+import json
 
 ticker = yf.Ticker('TSLA')
 tsla_df = ticker.history(period="max")
 tsla_df["Close"].plot(title="TSLA's stock price")
 
-tsla_df.iloc[::-1].to_csv(path_or_buf ="tesla.csv")
+#tsla_df.iloc[::-1].to_csv(path_or_buf ="tesla.csv")
+
+#creating info text file
+file = open('info.txt','w')
+dumped = json.dumps(ticker.info)
+
+file.write(dumped)
 
 
-plt.show()
+#plt.show()
