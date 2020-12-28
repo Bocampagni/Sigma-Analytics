0ee60311d284fe1642fb3759268e5038c4348fab
fabio.alves <fabio.alves@exablack.com>
2020-12-28 16:26:33 -0300

    modular methods added and main rewrited

--- main.py
+++ main.py
@@ -2,16 +2,12 @@ import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
 from info import getInfo
+from sincronousExtractor import fetchTickers
 
 tickers = yf.Tickers('TSLA GOOG AAPL')
 tickers_info = getInfo(tickers)
-
-#Matriz 2d, primeiro 0 corresponde a primeira lista, segundo zero corresponde ao primeiro elemento da primeira lista (Ticker)
-for x in range(3):
-    tickers_info[x][0].history(period="7d").to_csv("stockData.csv",mode='a', header=False)
-
-#Adicionar um separador no csv para sabermos exatamente o IOF do ticker
-
+data = fetchTickers(tickers_info,3).to_csv("stockData.csv")
+    
 #Adicionar gráficos dentro do csv para cada ticker
 
 #Enviar o email
