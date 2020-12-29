faf6de3972fa7351c2b1888adef0ba633684e6f0
fabio.alves <fabio.alves@exablack.com>
2020-12-29 20:41:44 -0300

    implemented: nodejs server, wip: email template

---
+++ index.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3000
+var cors = require('cors')
+
+
+
+app.use(cors())
+
+app.get('/getJson', (req, res) => {
+    fs.readFile('data.json', 'utf8', function (err, data) {
+        if (err) throw err;
+         res.send(JSON.parse(data));
+      });
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
