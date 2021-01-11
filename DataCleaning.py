13cd4dc7c586bb06cf5049e1b47053bd661da26c
fabio.alves <fabio.alves@exablack.com>
2021-01-11 18:22:12 -0300

    Mateus Rangel refactored: DataCleaning.py and I added packages.json

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,23 +1,12 @@
 import json
 
 def cleaningData(stockList,data):
+    with open('data.json', 'w', encoding='utf-8') as f:
+        output = []
+        for x in range(len(stockList)):
+            stock = [{"Ticker": stockList[x]}]
+            data[x] = data[x].reset_index()
+            stock.append(json.loads(data[x].to_json(orient="records",date_format="iso")))
+            output.append(stock)
 
-    file = open("data.json","w")
-    file.write("[")
-    for x in range(len(stockList)):
-        file.write("[")
-        dic_ticker = {"Ticker": stockList[x]}
-        data[x] = data[x].reset_index()
-        
-        file.write(json.dumps(dic_ticker))
-        file.write(",")
-        var = data[x].to_json(orient="records",date_format="iso")
-        parsed = json.loads(var)
-        
-        file.write(json.dumps(parsed, indent=4))
-        file.write("]")
-        if x != len(stockList) - 1:
-            file.write(",")
-        
-    file.write("]")
-    file.close()
+        json.dump(output, f, indent=4)
