5ae670d1630255af09304d1d207fd0aecebc04d6
fabio.alves <fabio.alves@exablack.com>
2021-01-23 00:04:21 -0300

    Fixed date time issue in template and implemented sys lib in main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import yfinance as yf
 import os
@@ -6,13 +7,22 @@ import json
 import glob
 from DataCleaning import cleaningData
 
-stockNames = 'TSLA GOOG AAPL'
-stockList = stockNames.split(" ")
 
+stockList = sys.argv
+stockList.pop(0) #Used to remove the first parameter ( file's name )
+
+stockNames = " ".join(stockList)
 tickers = yf.Tickers(stockNames)
 
+
+tickerNames = []
 data = []
-for ticker in tickers.tickers:
-    data.append(ticker.history(period="7d"))
+for index,ticker in enumerate(tickers.tickers):
+    returnedValue = ticker.history(period="7d")
+    if(returnedValue.size > 0):
+
+        data.append(returnedValue)
+    else:
+        stockList.pop(index)
 
 cleaningData(stockList,data)
